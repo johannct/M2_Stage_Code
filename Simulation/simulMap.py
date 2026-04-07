06c7360e34ce9594d5d463351844687748b8442e
Victoria NAPPA <yuyu.atem@gmail.com>
2026-04-07 18:43:03 +0200

    Ajout notebook Cluster_maps_Ntrial dans le dossier Simulation pour faire Nlancés de clustering et les sauvegarder dans un fichier externe.

--- Simulation/simulMap.py
+++ Simulation/simulMap.py
@@ -3,6 +3,7 @@
 ### Package importation:
 import numpy as np
 import healpy as hp
+import pandas as pd
 import matplotlib.pyplot as plt
 import astropy.units as u
 from astropy.coordinates import SkyCoord
@@ -163,6 +164,30 @@ def plot_fit(x_fit, y_fit, values, model, **kwargs):
     return fig, ax
 
 
+def get_LeastSquare_plot(least_square, param, index=-2, start=0, stop=1e7, step=10000, **kwargs):
+    '''Plot the LeastSquare function of a fit vs. param[index].'''
+    par = param.copy()
+    parx = np.linspace(start, stop, step)
+    ls = []
+    for x in parx:
+        par[index] = x
+        ls.append(least_square(*par))
+    
+    fig, ax = plt.subplots()
+    ax.plot(parx, ls)
+    xlabel = kwargs.get("xlabel", "param[{}]".format(index))
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel("$\chi_2$ function")
+    if "title" in kwargs.keys(): ax.set_title(kwargs["title"])
+    
+    output_path = kwargs.get("output_path", False)
+    if output_path:
+        ext = kwargs.get('format', 'pdf')
+        get_savefig(fig=fig, output_path=output_path, sufix="chi2_vs_param{}".format(index), format=ext)
+    
+    return ls
+
+
 ## Fit functions:
 def fit_minuit(x_fit, y_fit, y_err, model, init, par_name, bounds=None, fixed=[], get_fig=False, plot_fig=True, **kwargs):
     '''Fit data with iminuit and the least squares methode, and return the corresponding Minuit() instance.
@@ -178,6 +203,7 @@ def fit_minuit(x_fit, y_fit, y_err, model, init, par_name, bounds=None, fixed=[]
     Optionnal parameters:
     - get_fig: if True, return also the fig, ax variables from the data&fit figure. By default, get_fig=False.
     '''
+    verbose = kwargs.pop('verbose', True)
     cost_func = kwargs.pop("cost_func", LeastSquares(x_fit, y_fit, y_err, model))
     m = Minuit(cost_func, *init, name=par_name)
     if bounds: m.limits = bounds
@@ -185,7 +211,8 @@ def fit_minuit(x_fit, y_fit, y_err, model, init, par_name, bounds=None, fixed=[]
     m.migrad()       # finds minimum of least_squares function
     m.hesse()        # accurately computes uncertainties
     try: m.minos()   # computes non symetrics uncertainties
-    except: print('Unable to use minos()')
+    except:
+        if verbose: print('Unable to use minos()')
 
     if plot_fig: fig, ax = plot_fit(x_fit, y_fit, m.values, model, **kwargs)
     else: get_fig = False #fig, ax only exist if plot_fig = True, so they can't be returned if plot_fig = False.
@@ -202,8 +229,53 @@ def fit_dipole_err(model, map, init, names, bounds=([0, 0, -90], [1, 360, 90]), 
     #print("ipix =", ipix)
     nside= hp.npix2nside(npix)
 
-    if fit_mode == "minuit": return fit_minuit(ipix, map, map_errY, model, init, names, list(zip(bounds[0], bounds[1])), fixed, title="Fit dipole", xlabel="Pixels")
+    if fit_mode == "minuit": return fit_minuit(ipix, map, map_errY, model, init, names, list(zip(bounds[0], bounds[1])), fixed, title="Fit dipole", xlabel="Pixels", **kwargs)
     else: return curve_fit(model, ipix, map, p0=init, bounds=bounds, sigma= map_errY)
+
+
+def get_saveFit_minuit(m, output_path, sufix, x_fit=None, y_fit=None, y_fit_err=None, dic_param={}, index_col=None, verbose=True):
+    """Save the resulf of a fit frim iminuit into a CSV file from a pandas dataframe."""
+    #For list items, drop_duplicates() returns an error.
+    #Values will be saved as str, and adapted to be later reconstructed by eval().
+    def adapt_to_str(ch):
+        ch = repr(ch)
+        if str(ch) == "None": ch = "NaN" #easier to treat in DataFrame.
+        if "dtype" in ch: ch = ch[:ch.index("dtype")] +')'
+        if "array" in ch: ch = "np."+ch
+        return ch
+        
+    for k, v in m.values.to_dict().items():
+        dic_param[k] = v
+        dic_param[k + "_init"] = m.init_params[k].value
+        dic_param[k + "_err"] = m.errors[k]
+        dic_param[k + "_fixed"] = m.fixed[k]
+        dic_param[k + "_limits"] = [adapt_to_str(m.limits[k])]
+    dic_param["valid"] = m.valid
+    dic_param["x_fit"] = [adapt_to_str(x_fit)]
+    dic_param["y_fit"] = [adapt_to_str(y_fit)]
+    dic_param["y_fit_err"] = [adapt_to_str(y_fit_err)]
+    
+    table_param = pd.DataFrame(dic_param)
+    if index_col: table_param.set_index(index_col, inplace=True)
+
+    output_file = output_path + sufix + '.csv'
+    try:
+        if verbose: print("\nTrying to open Dataframe from", output_file)
+        table_param2 = pd.read_csv(output_file, index_col=index_col)
+        #if 'Unnamed: 0' in table_param2..columns: table_param2.pop('Unnamed: 0') #because a column can be added by the indices.
+        if verbose: print("Concatenating new and former dataframes.\n")
+        table_param2 = pd.concat([table_param, table_param2])
+        table_param2.drop_duplicates()
+    except:
+        if verbose:
+            print("No existing file found for the path", output_file)
+            print("Creating a new one.\n")
+        table_param2 = table_param
+        
+    if verbose: print('Saving Dataframe results in {}'.format(output_file))
+    has_name = table_param2.index.name is not None #to save the index column only if it has a name.
+    table_param2.to_csv(output_file, index=has_name)
+    return table_param
 
     
 ## Fit models:
